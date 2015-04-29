d7ac6ee5d0398dd7a4f144f2288212dc6b82beea
gegy1000 <gegy1000@gmail.com>
2015-04-29 17:59:01 +0200

    Json animations

--- src/main/java/net/ilexiconn/jurassicraft/client/model/animation/TestAnimator.java
+++ src/main/java/net/ilexiconn/jurassicraft/client/model/animation/TestAnimator.java
@@ -8,6 +8,10 @@ public class TestAnimator implements IModelAnimator
 {
     public void setRotationAngles(ModelJson model, float limbSwing, float limbSwingAmount, float rotation, float rotationYaw, float rotationPitch, float partialTicks, Entity entity)
     {
-        model.faceTarget(model.getCube("head"), 1, rotationYaw, rotationPitch);
+//        model.faceTarget(model.getCube("head"), 1, rotationYaw, rotationPitch);
+    	if(!model.isAnimationInProgress())
+    	{
+    		model.startAnimation(0);
+    	}
     }
 }
