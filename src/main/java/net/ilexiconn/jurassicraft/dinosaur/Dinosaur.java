3d77897de2fb3307995a02c60562eaef023ae5f3
rjwallace@outlook.com <rjwallace@outlook.com>
2015-05-20 00:37:21 -0500

    *Fix

--- src/main/java/net/ilexiconn/jurassicraft/dinosaur/Dinosaur.java
+++ src/main/java/net/ilexiconn/jurassicraft/dinosaur/Dinosaur.java
@@ -21,6 +21,8 @@ import org.apache.commons.io.IOUtils;
 
 public abstract class Dinosaur
 {
+    public abstract String getName();
+
     public abstract Class<? extends EntityDinosaur> getDinosaurClass();
     
     public abstract int getEggPrimaryColor();
