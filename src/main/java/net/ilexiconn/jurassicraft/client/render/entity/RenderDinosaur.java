8e154ee0571139083605082a7612fdf9c11ac0c2
gegy1000 <gegy1000@gmail.com>
2015-06-08 08:58:29 +0200

    Forgot one file

--- src/main/java/net/ilexiconn/jurassicraft/client/render/entity/RenderDinosaur.java
+++ src/main/java/net/ilexiconn/jurassicraft/client/render/entity/RenderDinosaur.java
@@ -30,20 +30,20 @@ public class RenderDinosaur extends RenderMultiPart
         super(creature.getModel(), creature.getShadowSize());
         dino = creature;
         random = new Random();
-        
+
         maleTextures = new ResourceLocation[creature.getMaleTextures().length];
         femaleTextures = new ResourceLocation[creature.getFemaleTextures().length];
-        
+
         int i = 0;
-        
+
         for (String texture : creature.getMaleTextures())
         {
             maleTextures[i] = new ResourceLocation(texture);
             i++;
         }
-        
+
         i = 0;
-        
+
         for (String texture : creature.getFemaleTextures())
         {
             femaleTextures[i] = new ResourceLocation(texture);
@@ -53,7 +53,7 @@ public class RenderDinosaur extends RenderMultiPart
 
     public void preRenderCallback(EntityLivingBase entity, float side)
     {
-        float scale = dino.getScaleAdjustment(); //ModelBiped
+        float scale = dino.getScaleAdjustment();
         shadowSize = scale * dino.getShadowSize();
         if (entity instanceof EntityVelociraptor && (entity.getCustomNameTag().equals("iLexiconn") || entity.getCustomNameTag().equals("JTGhawk137")))
         {
@@ -64,20 +64,20 @@ public class RenderDinosaur extends RenderMultiPart
             GL11.glScalef(scale, scale, scale);
         }
 
-        if(entity.getCustomNameTag().equals("Gegy"))
+        if (entity.getCustomNameTag().equals("Gegy"))
         {
             int ticksExisted = entity.ticksExisted / 25 + entity.getEntityId();
             int colorTypes = EnumDyeColor.values().length;
-            int k = ticksExisted % colorTypes; 
+            int k = ticksExisted % colorTypes;
             int l = (ticksExisted + 1) % colorTypes;
-            float time = ((float)(entity.ticksExisted % 25) + 2) / 25.0F;
+            float time = ((float) (entity.ticksExisted % 25) + 2) / 25.0F;
             float[] colors = EntitySheep.func_175513_a(EnumDyeColor.byMetadata(k));
             float[] colors2 = EntitySheep.func_175513_a(EnumDyeColor.byMetadata(l));
             GlStateManager.color(colors[0] * (1.0F - time) + colors2[0] * time, colors[1] * (1.0F - time) + colors2[1] * time, colors[2] * (1.0F - time) + colors2[2] * time);
-           
-            if(time > 0.5F)
+
+            if (time > 0.5F)
                 time = 1 - time;
-            
+
             GL11.glScalef(1, 1 + time * 0.5F, 1);
         }
     }
