455aa912b5315ea5f3029f82b935b356a70c0405
gegy1000 <gegy1000@gmail.com>
2015-07-02 13:34:03 +0200

    Dinosaur base types

---
+++ src/main/java/net/timeless/jurassicraft/entity/base/EntityDinosaurFlying.java
@@ -0,0 +1,16 @@
+package net.timeless.jurassicraft.entity.base;
+
+import net.minecraft.world.World;
+
+public class EntityDinosaurFlying extends EntityDinosaur
+{
+    public EntityDinosaurFlying(World world)
+    {
+        super(world);
+    }
+    
+    public void fall(float f, float damageMultiplier)
+    {
+        //TODO slow itself down when landing, if falling too fast, take damage
+    }
+}
---
+++ src/main/java/net/timeless/jurassicraft/entity/base/EntityDinosaurFlyingAggressive.java
@@ -0,0 +1,16 @@
+package net.timeless.jurassicraft.entity.base;
+
+import net.minecraft.world.World;
+
+public class EntityDinosaurFlyingAggressive extends EntityDinosaurAggressive
+{
+    public EntityDinosaurFlyingAggressive(World world)
+    {
+        super(world);
+    }
+    
+    public void fall(float f, float damageMultiplier)
+    {
+        //TODO slow itself down when landing, if falling too fast, take damage
+    }
+}
