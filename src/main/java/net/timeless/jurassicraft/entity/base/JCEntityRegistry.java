bb8906bf458a3c61e440dcb5aa0043480857922e
gegy1000 <gegy1000@gmail.com>
2015-07-08 09:01:27 +0200

    Revert "Revert "Fixed Ankylosaurus""
    This reverts commit 9ea82689b2b2cd64bc6a911967307894d6fb0d2b.

--- src/main/java/net/timeless/jurassicraft/entity/base/JCEntityRegistry.java
+++ src/main/java/net/timeless/jurassicraft/entity/base/JCEntityRegistry.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.timeless.jurassicraft.JurassiCraft;
 import net.timeless.jurassicraft.dinosaur.Dinosaur;
 import net.timeless.jurassicraft.dinosaur.DinosaurAchillobator;
-import net.timeless.jurassicraft.dinosaur.DinosaurAnklyosaurus;
+import net.timeless.jurassicraft.dinosaur.DinosaurAnkylosaurus;
 import net.timeless.jurassicraft.dinosaur.DinosaurCarnotaurus;
 import net.timeless.jurassicraft.dinosaur.DinosaurCompsognathus;
 import net.timeless.jurassicraft.dinosaur.DinosaurDilophosaurus;
@@ -36,7 +36,7 @@ public class JCEntityRegistry
     private static HashMap<EnumTimePeriod, List<Dinosaur>> dinosaursFromPeriod = new HashMap<>();
 
     public static final Dinosaur achillobator = new DinosaurAchillobator();
-    public static final Dinosaur anklyosaurus = new DinosaurAnklyosaurus();
+    public static final Dinosaur anklyosaurus = new DinosaurAnkylosaurus();
     public static final Dinosaur carnotaurus = new DinosaurCarnotaurus();
     public static final Dinosaur compsognathus = new DinosaurCompsognathus();
     public static final Dinosaur dilophosaurus = new DinosaurDilophosaurus();
