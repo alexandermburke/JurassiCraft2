da36bc5f0b56f49be73096c5cde2470658b09d25
gegy1000 <gegy1000@gmail.com>
2015-07-24 07:47:16 +0200

    P2

--- src/main/java/net/timeless/jurassicraft/client/model/animation/ai/JCAutoAnimBase.java
+++ src/main/java/net/timeless/jurassicraft/client/model/animation/ai/JCAutoAnimBase.java
@@ -1,26 +1,21 @@
-package net.timeless.jurassicraft.client.model.animation.ai2;
+package net.timeless.jurassicraft.client.model.animation.ai;
 
 import net.timeless.animationapi.AIAnimation;
 import net.timeless.animationapi.IAnimatedEntity;
 import net.timeless.jurassicraft.common.entity.base.EntityDinosaur;
 
-/**
- * Created by jnad325 on 7/23/15.
- */
-public class JCNonAutoAnimBase extends AIAnimation
+public abstract class JCAutoAnimBase extends AIAnimation
 {
     protected EntityDinosaur animatingEntity;
     protected int duration;
     protected int id;
-    protected int chance;
 
-    public JCNonAutoAnimBase(IAnimatedEntity entity, int duration, int id, int chance)
+    public JCAutoAnimBase(IAnimatedEntity entity, int duration, int id)
     {
         super(entity);
         this.duration = duration;
         animatingEntity = (EntityDinosaur) entity;
         this.id = id;
-        this.chance = chance;
     }
 
     @Override
@@ -30,17 +25,12 @@ public class JCNonAutoAnimBase extends AIAnimation
 
     public boolean isAutomatic()
     {
-        return false;
+        return true;
     }
 
     public int getDuration()
     {
         return duration;
-    }
-
-    @Override
-    public boolean shouldExecute() {
-        return animatingEntity.getAnimID() == 0 && animatingEntity.getRNG().nextInt(chance) == 0;
     }
 
     public void startExecuting()
