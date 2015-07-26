84b77aecd79241f92d23fd3d16c7a399fe08e4a2
rjwallace@outlook.com <rjwallace@outlook.com>
2015-07-26 08:09:02 -0500

    AI Food Update

--- src/main/java/net/reuxertz/ecoapi/EcoAPI.java
+++ src/main/java/net/reuxertz/ecoapi/EcoAPI.java
@@ -1,5 +1,7 @@
 package net.reuxertz.ecoapi;
 
+import net.minecraft.entity.passive.EntityRabbit;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.reuxertz.ecoapi.ecology.EcoFauna;
 import net.reuxertz.ecoapi.ecology.role.IEcologicalRole;
@@ -15,5 +17,10 @@ public class EcoAPI
     public static void registerEntityClassDropItems(Class entityClass, ItemStack[] dropItems)
     {
         EcoFauna.addToClassDropRegistry(entityClass, dropItems);
+    }
+
+    public static void registerFoodItem(IEcologicalRole role, ItemStack stack)
+    {
+        EcoAPI.registerEntityClassDropItems(EntityRabbit.class,     new ItemStack[]     { stack.copy()   });
     }
 }
