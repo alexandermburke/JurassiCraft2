456cdbb614ed0dc713205e57b0684c6518a9b1bc
gegy1000 <gegy1000@gmail.com>
2015-08-08 21:59:57 +0200

    Work more on the renderer

--- src/main/java/net/timeless/jurassicraft/client/render/tileentity/TileEntityRendererCage.java
+++ src/main/java/net/timeless/jurassicraft/client/render/tileentity/TileEntityRendererCage.java
@@ -6,9 +6,8 @@ import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.tileentity.TileEntity;
-import net.timeless.jurassicraft.common.entity.EntityAchillobator;
-import net.timeless.jurassicraft.common.entity.EntityBrachiosaurus;
-import net.timeless.jurassicraft.common.entity.EntityHypsilophodon;
+import net.minecraft.util.EnumFacing;
+import net.timeless.jurassicraft.common.entity.*;
 import net.timeless.jurassicraft.common.tileentity.TileCage;
 
 public class TileEntityRendererCage extends TileEntitySpecialRenderer
@@ -26,7 +25,7 @@ public class TileEntityRendererCage extends TileEntitySpecialRenderer
 
         if(entity == null)
         {
-            entity = new EntityHypsilophodon(tile.getWorld());
+            entity = new EntityGallimimus(tile.getWorld());
             cage.setEntity(entity);
         }
 
@@ -34,8 +33,15 @@ public class TileEntityRendererCage extends TileEntitySpecialRenderer
 
         float f1 = 0.5F;
         GlStateManager.translate(0.0F, 0.4F, 0.0F);
-//            GlStateManager.rotate((float)(mobSpawnerLogic.getPrevMobRotation() + (mobSpawnerLogic.getMobRotation() - mobSpawnerLogic.getPrevMobRotation()) * (double)partialTicks) * 10.0F, 0.0F, 1.0F, 0.0F);
-//            GlStateManager.rotate(-30.0F, 1.0F, 0.0F, 0.0F);
+        EnumFacing front = EnumFacing.getFront(tile.getBlockMetadata());
+
+        if(front == EnumFacing.EAST)
+            front = EnumFacing.WEST;
+        else if(front == EnumFacing.WEST)
+            front = EnumFacing.EAST;
+
+        System.out.println(front);
+        GlStateManager.rotate((front.getHorizontalIndex()) * 90, 0, 1, 0);
         GlStateManager.translate(0.0F, -0.4F, 0.0F);
         entity.setLocationAndAngles(posX, posY, posZ, 0.0F, 0.0F);
         Minecraft.getMinecraft().getRenderManager().renderEntityWithPosYaw(entity, 0.0D, 0.0D, 0.0D, 0.0F, 0.0F);
