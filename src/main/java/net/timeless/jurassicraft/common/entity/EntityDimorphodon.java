36403d2a6beaaccb2db7d928e942bed848c819f3
gegy1000 <gegy1000@gmail.com>
2015-08-09 17:02:54 +0200

    Forgot that

--- src/main/java/net/timeless/jurassicraft/common/entity/EntityDimorphodon.java
+++ src/main/java/net/timeless/jurassicraft/common/entity/EntityDimorphodon.java
@@ -1,11 +1,15 @@
 package net.timeless.jurassicraft.common.entity;
 
 import net.minecraft.world.World;
+import net.reuxertz.ecoapi.ecology.role.ICarnivore;
 import net.reuxertz.ecoapi.ecology.role.IHerbivore;
 import net.reuxertz.ecoapi.entity.IEntityAICreature;
+import net.reuxertz.ecoapi.entity.IEntityAIFlyingCreature;
 import net.timeless.jurassicraft.common.entity.base.EntityDinosaurDefensiveHerbivore;
+import net.timeless.jurassicraft.common.entity.base.EntityDinosaurFlying;
+import net.timeless.jurassicraft.common.entity.base.EntityDinosaurFlyingAggressive;
 
-public class EntityDimorphodon extends EntityDinosaurDefensiveHerbivore implements IEntityAICreature, IHerbivore
+public class EntityDimorphodon extends EntityDinosaurFlyingAggressive implements IEntityAIFlyingCreature, ICarnivore
 {
     public EntityDimorphodon(World world)
     {
