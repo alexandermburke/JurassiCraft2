a7dd286bcc7db44d38dbdfba8b2fa87551547491
TheLarsinator <TheLarsinatormodding@hotmail.com>
2015-08-10 00:25:13 +0200

    Animated GUI Syringe

--- src/main/java/net/timeless/jurassicraft/client/gui/GuiEmbryoCalcificationMachine.java
+++ src/main/java/net/timeless/jurassicraft/client/gui/GuiEmbryoCalcificationMachine.java
@@ -48,7 +48,16 @@ public class GuiEmbryoCalcificationMachine extends GuiContainer
         this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
 
         int progress = this.getProgress(24);
+        int progress1 = this.getProgress(9);
+        int progress2 = this.getProgress(20);
         this.drawTexturedModalRect(k + 67, l + 31, 176, 14, progress + 1, 16);
+       //Syringe Top
+        this.drawTexturedModalRect(k + 38, l + 32, 177, 32, 9, progress1);
+       //Syringe Inside  
+        this.drawTexturedModalRect(k + 38, l + 38, 197, 38, 9, progress2);
+       //Clean up 
+        this.drawTexturedModalRect(k + 38, l + 32, 187, 32, 9, progress1-1);
+ 
     }
 
     private int getProgress(int scale)
