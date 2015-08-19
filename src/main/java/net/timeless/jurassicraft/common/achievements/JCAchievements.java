2ba53f6ae368f3112bd5bc594139f37cefe5b80f
gegy1000 <gegy1000@gmail.com>
2015-08-19 15:01:27 +0200

    Some more achievements

--- src/main/java/net/timeless/jurassicraft/common/achievements/JCAchievements.java
+++ src/main/java/net/timeless/jurassicraft/common/achievements/JCAchievements.java
@@ -1,6 +1,5 @@
 package net.timeless.jurassicraft.common.achievements;
 
-import net.minecraft.stats.Achievement;
 import net.minecraftforge.common.AchievementPage;
 import net.timeless.jurassicraft.common.block.JCBlockRegistry;
 import net.timeless.jurassicraft.common.item.JCItemRegistry;
@@ -11,17 +10,19 @@ public class JCAchievements
     public static JCAchievement fossils;
     public static JCAchievement paleontology;
     public static JCAchievement amber;
+    public static JCAchievement cleaningStation;
 
     public static AchievementPage jurassicraftPage;
 
     public void register()
     {
         jurassicraft = (JCAchievement) (new JCAchievement("mod", 0, 0, JCItemRegistry.skull, null)).initIndependentStat();
-        paleontology = new JCAchievement("paleontology", 1, 1, JCItemRegistry.plaster_and_bandage, jurassicraft);
-        fossils = new JCAchievement("fossils", 3, 2, JCBlockRegistry.encased_fossils.get(0), paleontology);
+        paleontology = new JCAchievement("paleontology", 2, 1, JCItemRegistry.plaster_and_bandage, jurassicraft);
+        fossils = new JCAchievement("fossils", 3, 3, JCBlockRegistry.encased_fossils.get(0), paleontology);
         amber = new JCAchievement("amber", 2, -2, JCItemRegistry.amber, jurassicraft);
+        cleaningStation = new JCAchievement("cleaningStation", 0, 2, JCBlockRegistry.cleaning_station, jurassicraft);
 
-        jurassicraftPage = new AchievementPage("JurassiCraft", jurassicraft, paleontology, fossils, amber);
+        jurassicraftPage = new AchievementPage("JurassiCraft", jurassicraft, paleontology, fossils, amber, cleaningStation);
 
         AchievementPage.registerAchievementPage(jurassicraftPage);
     }
