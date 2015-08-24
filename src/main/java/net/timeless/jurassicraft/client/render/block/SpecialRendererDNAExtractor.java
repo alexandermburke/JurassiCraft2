15014118b27f14b321abfef6f91811c19e2bcb83
gegy1000 <gegy1000@gmail.com>
2015-08-24 19:43:57 +0200

    Some optimizations

--- src/main/java/net/timeless/jurassicraft/client/render/block/SpecialRendererDNAExtractor.java
+++ src/main/java/net/timeless/jurassicraft/client/render/block/SpecialRendererDNAExtractor.java
@@ -13,13 +13,15 @@ import net.timeless.jurassicraft.common.tileentity.TileDNAExtractor;
 
 public class SpecialRendererDNAExtractor extends TileEntitySpecialRenderer
 {
+    private Minecraft mc = Minecraft.getMinecraft();
+
     @Override
     public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float p_180535_8_, int p_180535_9_)
     {
         TileDNAExtractor tile = (TileDNAExtractor) tileEntity;
 
         GlStateManager.pushMatrix();
-        Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.locationBlocksTexture);
+        mc.getTextureManager().bindTexture(TextureMap.locationBlocksTexture);
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         GlStateManager.translate(x + 0.5, y, z + 0.5);
 
@@ -40,12 +42,13 @@ public class SpecialRendererDNAExtractor extends TileEntitySpecialRenderer
         GlStateManager.scale(1.25F, 1.25F, 1.25F);
         GlStateManager.rotate(90, 1, 0, 0);
 
-        ItemStack stack = tile.getStackInSlot(0);
+        ItemStack extraction = tile.getStackInSlot(0);
 
-        if(stack != null)
+        RenderItem renderItem = mc.getRenderItem();
+
+        if(extraction != null)
         {
-            RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
-            renderItem.renderItem(stack, renderItem.getItemModelMesher().getItemModel(stack));
+            renderItem.renderItem(extraction, renderItem.getItemModelMesher().getItemModel(extraction));
         }
 
         ItemStack disc = tile.getStackInSlot(1);
@@ -57,10 +60,9 @@ public class SpecialRendererDNAExtractor extends TileEntitySpecialRenderer
 
             if(tile.isExtracting())
             {
-                GlStateManager.rotate(Minecraft.getMinecraft().thePlayer.ticksExisted * 2 % 360, 0, 0, 1);
+                GlStateManager.rotate(mc.thePlayer.ticksExisted * 2 % 360, 0, 0, 1);
             }
 
-            RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
             renderItem.renderItem(disc, renderItem.getItemModelMesher().getItemModel(disc));
         }
 
