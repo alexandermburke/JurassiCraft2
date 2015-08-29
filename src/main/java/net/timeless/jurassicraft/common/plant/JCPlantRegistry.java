06c9faa1106d0fc339db3c7231efe41d7be21897
gegy1000 <gegy1000@gmail.com>
2015-08-29 08:29:52 +0200

    Scaly tree fern plant

--- src/main/java/net/timeless/jurassicraft/common/plant/JCPlantRegistry.java
+++ src/main/java/net/timeless/jurassicraft/common/plant/JCPlantRegistry.java
@@ -14,6 +14,7 @@ public class JCPlantRegistry
     public static final Plant ginkgo = new PlantGinkgo();
     public static final Plant bennettitalean_cycadeoidea = new PlantBennettitaleanCycadeoidea();
     public static final Plant cry_pansy = new PlantCryPansy();
+    public static final Plant scaly_tree_fern = new PlantScalyTreeFern();
 
     public void register()
     {
@@ -24,6 +25,7 @@ public class JCPlantRegistry
         registerPlant(ginkgo);
         registerPlant(bennettitalean_cycadeoidea);
         registerPlant(cry_pansy);
+        registerPlant(scaly_tree_fern);
     }
 
     public static Plant getPlantById(int id)
