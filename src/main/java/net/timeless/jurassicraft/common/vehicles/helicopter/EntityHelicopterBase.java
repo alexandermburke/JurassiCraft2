7b1013b2fcd27a5bd89736d6e7897ffba62d69fe
gegy1000 <gegy1000@gmail.com>
2015-08-29 19:26:42 +0200

    Revert "Helicopters aren't living"
    This reverts commit 23ed942a226f524c73db06a46926ead6efd84041.

--- src/main/java/net/timeless/jurassicraft/common/vehicles/helicopter/EntityHelicopterBase.java
+++ src/main/java/net/timeless/jurassicraft/common/vehicles/helicopter/EntityHelicopterBase.java
@@ -1,12 +1,13 @@
 package net.timeless.jurassicraft.common.vehicles.helicopter;
 
-import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.*;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 import javax.vecmath.Vector2f;
@@ -14,7 +15,7 @@ import javax.vecmath.Vector2f;
 /**
  * Base entity for the helicopter, also holds the {@link HelicopterSeat Seat} entities and updates/handles them.
  */
-public class EntityHelicopterBase extends Entity
+public class EntityHelicopterBase extends EntityLivingBase
 {
     public HelicopterSeat[] seats;
     public static final int PILOT_SEAT = 0;
@@ -63,11 +64,31 @@ public class EntityHelicopterBase extends Entity
     @Override
     protected void entityInit()
     {
+        super.entityInit();
     }
 
     @Override
     public void readEntityFromNBT(NBTTagCompound tagCompound)
     {
+        super.readEntityFromNBT(tagCompound);
+    }
+
+    @Override
+    public ItemStack getHeldItem()
+    {
+        return null;
+    }
+
+    @Override
+    public ItemStack getEquipmentInSlot(int slotIn)
+    {
+        return null;
+    }
+
+    @Override
+    public ItemStack getCurrentArmor(int slotIn)
+    {
+        return null;
     }
 
     @Override
@@ -85,18 +106,13 @@ public class EntityHelicopterBase extends Entity
     @Override
     public void writeEntityToNBT(NBTTagCompound tagCompound)
     {
+        super.writeEntityToNBT(tagCompound);
     }
 
     @Override
-    public void onUpdate()
+    public void onLivingUpdate()
     {
-        super.onUpdate();
-        motionY -= 0.2D;
-
-        motionY *= 0.95;
-
-        moveEntity(motionX, motionY, motionZ);
-
+        super.onLivingUpdate();
         ignoreFrustumCheck = true; // always draws the entity
         // Update seats positions
         for(HelicopterSeat seat : seats)
@@ -175,30 +191,12 @@ public class EntityHelicopterBase extends Entity
         return entityIn.getEntityBoundingBox();
     }
 
-    public boolean attackEntityFrom(DamageSource source, float amount)
-    {
-        if(!isEntityInvulnerable(source))
-        {
-            this.setDead();
-        }
-
-        return super.attackEntityFrom(source, amount);
-    }
-
     /**
      * returns the bounding box for this entity
      */
     public AxisAlignedBB getBoundingBox()
     {
         return this.getEntityBoundingBox();
-    }
-
-    /**
-     * Returns true if other Entities should be prevented from moving through this Entity.
-     */
-    public boolean canBeCollidedWith()
-    {
-        return false;
     }
 
     /**
@@ -209,88 +207,9 @@ public class EntityHelicopterBase extends Entity
         return false;
     }
 
-    /**
-     * Called when a player mounts an entity. e.g. mounts a pig, mounts a boat.
-     */
-    public void mountEntity(Entity entityIn)
+    public void collideWithNearbyEntities()
     {
-        if(!net.minecraftforge.event.ForgeEventFactory.canMountEntity(this, entityIn, true)){ return; }
-        if (this.ridingEntity != null && entityIn == null)
-        {
-            if (!this.worldObj.isRemote)
-            {
-                this.dismountEntity(this.ridingEntity);
-            }
 
-            if (this.ridingEntity != null)
-            {
-                this.ridingEntity.riddenByEntity = null;
-            }
-
-            this.ridingEntity = null;
-        }
-        else
-        {
-            super.mountEntity(entityIn);
-        }
-    }
-
-    /**
-     * Moves the entity to a position out of the way of its mount.
-     */
-    public void dismountEntity(Entity p_110145_1_)
-    {
-        if(!net.minecraftforge.event.ForgeEventFactory.canMountEntity(this, p_110145_1_, false)){ return; }
-        double d0 = p_110145_1_.posX;
-        double d1 = p_110145_1_.getEntityBoundingBox().minY + (double)p_110145_1_.height;
-        double d2 = p_110145_1_.posZ;
-        byte b0 = 1;
-
-        for (int i = -b0; i <= b0; ++i)
-        {
-            for (int j = -b0; j < b0; ++j)
-            {
-                if (i != 0 || j != 0)
-                {
-                    int k = (int)(this.posX + (double)i);
-                    int l = (int)(this.posZ + (double)j);
-                    AxisAlignedBB axisalignedbb = this.getEntityBoundingBox().offset((double)i, 1.0D, (double)j);
-
-                    if (this.worldObj.func_147461_a(axisalignedbb).isEmpty())
-                    {
-                        if (World.doesBlockHaveSolidTopSurface(this.worldObj, new BlockPos(k, (int)this.posY, l)))
-                        {
-                            this.setPositionAndUpdate(this.posX + (double)i, this.posY + 1.0D, this.posZ + (double)j);
-                            return;
-                        }
-
-                        if (World.doesBlockHaveSolidTopSurface(this.worldObj, new BlockPos(k, (int)this.posY - 1, l)) || this.worldObj.getBlockState(new BlockPos(k, (int)this.posY - 1, l)).getBlock().getMaterial() == Material.water)
-                        {
-                            d0 = this.posX + (double)i;
-                            d1 = this.posY + 1.0D;
-                            d2 = this.posZ + (double)j;
-                        }
-                    }
-                }
-            }
-        }
-
-        this.setPositionAndUpdate(d0, d1, d2);
-    }
-
-    /**
-     * Sets the x,y,z of the entity from the given parameters. Also seems to set up a bounding box.
-     */
-    public void setPosition(double x, double y, double z)
-    {
-        super.setPosition(x, y, z);
-    }
-
-    /**
-     * Applies a velocity to each of the entities pushing them away from each other. Args: entity
-     */
-    public void applyEntityCollision(Entity entityIn)
-    {
     }
 
     public float getRoll()
