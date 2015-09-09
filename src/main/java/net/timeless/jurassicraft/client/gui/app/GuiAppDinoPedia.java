331181283260ed3370598c4e68d7398cc36fdffd
gegy1000 <gegy1000@gmail.com>
2015-09-09 17:15:21 +0200

    More work on DinoPedia

--- src/main/java/net/timeless/jurassicraft/client/gui/app/GuiAppDinoPedia.java
+++ src/main/java/net/timeless/jurassicraft/client/gui/app/GuiAppDinoPedia.java
@@ -1,15 +1,12 @@
 package net.timeless.jurassicraft.client.gui.app;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderItem;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.ShapedRecipes;
@@ -19,7 +16,6 @@ import net.timeless.jurassicraft.client.gui.GuiPaleoTab;
 import net.timeless.jurassicraft.common.dinopedia.DinoPediaRegistry;
 import net.timeless.jurassicraft.common.paleopad.App;
 import net.timeless.jurassicraft.common.paleopad.AppDinoPedia;
-import net.timeless.jurassicraft.common.world.jurdstrees.algorythms.Shape;
 
 import java.util.List;
 
@@ -30,6 +26,8 @@ public class GuiAppDinoPedia extends GuiApp
     private RenderItem render = Minecraft.getMinecraft().getRenderItem();
 
     private boolean intro;
+
+    private int scroll;
 
     public GuiAppDinoPedia(App app)
     {
@@ -55,83 +53,129 @@ public class GuiAppDinoPedia extends GuiApp
         {
             gui.drawScaledText("Items:", 4, 5, 1.0F, 0xFFFFFF);
 
+            drawItemsAndRecipes(gui, app);
+        }
+    }
+
+    private void drawItemsAndRecipes(GuiPaleoTab gui, AppDinoPedia app)
+    {
+        List<ItemStack> items = DinoPediaRegistry.getItems();
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.disableLighting();
+
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+
+        RenderHelper.enableGUIStandardItemLighting();
+
+        for (int i = 0; i < items.size(); i++)
+        {
+            ItemStack stack = items.get(i);
+
+            if(stack != null && i >= scroll && i < 7 + scroll)
+            {
+                GlStateManager.pushMatrix();
+
+                int renderY = (i - scroll) * 18 + 10;
+
+                if(app.getSelectedItem() == i)
+                {
+                    gui.drawBoxOutline(4, renderY + 8, 18, 18, 1, 1.0F, 0x606060);
+                }
+
+                ScaledResolution dimensions = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
+                GlStateManager.translate(dimensions.getScaledWidth() / 2 - 115, 75, 0);
+
+                render.zLevel = 200.0F;
+                render.renderItemAndEffectIntoGUI(stack, 5, renderY);
+                render.zLevel = 0.0F;
+
+                if(app.getSelectedItem() == i)
+                {
+                    GlStateManager.translate(70, 10, 0);
+
+                    List<IRecipe> recipes = DinoPediaRegistry.getRecipesForItem(stack);
+
+                    for (IRecipe recipe : recipes)
+                    {
+                        if (recipe instanceof ShapedRecipes)
+                        {
+                            ShapedRecipes shapedRecipe = (ShapedRecipes) recipe;
+
+                            ItemStack[] recipeItems = shapedRecipe.recipeItems;
+
+                            int rx = 0;
+                            int ry = 0;
+
+                            for (int r = 0; r < recipeItems.length; r++)
+                            {
+                                ItemStack item = recipeItems[r];
+
+                                gui.drawBoxOutline(rx * 19 - 206, ry * 19 - 66, 18, 18, 1, 1.0F, 0x606060);
+
+                                if (item != null)
+                                {
+                                    render.zLevel = 200.0F;
+                                    render.renderItemAndEffectIntoGUI(item, rx * 19, ry * 19);
+                                    render.zLevel = 0.0F;
+                                }
+
+                                rx++;
+
+                                if (rx >= 3)
+                                {
+                                    rx = 0;
+                                    ry++;
+                                }
+                            }
+
+                            break;
+                        }
+                    }
+                }
+
+                GlStateManager.popMatrix();
+            }
+        }
+
+        GlStateManager.enableLighting();
+        GlStateManager.enableDepth();
+    }
+
+
+    @Override
+    public void mouseClicked(int mouseX, int mouseY, GuiPaleoTab gui)
+    {
+        ScaledResolution dimensions = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
+        mouseX -= dimensions.getScaledWidth() / 2 - 115;
+        mouseY -= 75;
+
+        if (intro)
+        {
+
+        }
+        else
+        {
+            AppDinoPedia app = (AppDinoPedia) getApp();
+
             List<ItemStack> items = DinoPediaRegistry.getItems();
-            RenderHelper.disableStandardItemLighting();
-            GlStateManager.disableLighting();
-
-            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-
-            RenderHelper.enableGUIStandardItemLighting();
 
             for (int i = 0; i < items.size(); i++)
             {
                 ItemStack stack = items.get(i);
 
-                if(stack != null && i < 7)
+                if (stack != null && i >= scroll && i < 7 + scroll)
                 {
-                    GlStateManager.pushMatrix();
+                    int y = (i - scroll) * 18 + 10;
 
-                    int renderY = i * 18 + 10;
-
-                    if(app.getSelectedItem() == i)
+                    if(mouseX > 0 && mouseX < 22 && mouseY > y && mouseY < y + 18)
                     {
-                        gui.drawBoxOutline(4, renderY + 8, 18, 18, 1, 1.0F, 0x606060);
+                        app.setSelectedItem(i);
+
+                        break;
                     }
-
-                    ScaledResolution dimensions = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
-                    GlStateManager.translate(dimensions.getScaledWidth() / 2 - 115, 75, 0);
-
-                    render.zLevel = 200.0F;
-                    render.renderItemAndEffectIntoGUI(stack, 5, renderY);
-                    render.zLevel = 0.0F;
-
-                    if(app.getSelectedItem() == i)
-                    {
-                        GlStateManager.translate(70, 10, 0);
-
-                        List<IRecipe> recipes = DinoPediaRegistry.getRecipesForItem(stack);
-
-                        for (IRecipe recipe : recipes)
-                        {
-                            if (recipe instanceof ShapedRecipes)
-                            {
-                                ShapedRecipes shapedRecipe = (ShapedRecipes) recipe;
-
-                                ItemStack[] recipeItems = shapedRecipe.recipeItems;
-
-                                int rx = 0;
-                                int ry = 0;
-
-                                for (int r = 0; r < recipeItems.length; r++)
-                                {
-                                    ItemStack item = recipeItems[r];
-
-                                    if (item != null)
-                                    {
-                                        render.zLevel = 200.0F;
-                                        render.renderItemAndEffectIntoGUI(item, rx * 19, ry * 19);
-                                        render.zLevel = 0.0F;
-                                    }
-
-                                    rx++;
-
-                                    if (rx >= 3)
-                                    {
-                                        rx = 0;
-                                        ry++;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    GlStateManager.popMatrix();
                 }
             }
-
-            GlStateManager.enableLighting();
-            GlStateManager.enableDepth();
         }
     }
 
