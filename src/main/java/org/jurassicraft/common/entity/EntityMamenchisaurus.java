c29ccdef6c85c29cb68651fe7ed7dbc2e3422348
gegy1000 <gegy1000@gmail.com>
2015-09-28 17:56:45 +0200

    this is messed up
    sorry

---
+++ src/main/java/org/jurassicraft/common/entity/EntityMamenchisaurus.java
@@ -0,0 +1,12 @@
+package org.jurassicraft.common.entity;
+
+import net.minecraft.world.World;
+import org.jurassicraft.common.entity.base.EntityDinosaurDefensiveHerbivore;
+
+public class EntityMamenchisaurus extends EntityDinosaurDefensiveHerbivore
+{
+    public EntityMamenchisaurus(World world)
+    {
+        super(world);
+    }
+}
