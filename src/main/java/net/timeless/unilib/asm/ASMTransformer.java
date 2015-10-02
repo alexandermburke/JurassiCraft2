c23277c26379b5cab59fd7433792a15edf257c6f
xavpok <jglrxavpok@gmail.com>
2015-10-02 20:34:24 +0200

    Forgot imports

--- src/main/java/net/timeless/unilib/asm/ASMTransformer.java
+++ src/main/java/net/timeless/unilib/asm/ASMTransformer.java
@@ -1,22 +1,26 @@
 package net.timeless.unilib.asm;
 
 import com.google.common.collect.Lists;
+import net.minecraft.client.Minecraft;
 import net.minecraft.launchwrapper.IClassTransformer;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.registry.GameData;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.*;
+import org.objectweb.asm.util.CheckClassAdapter;
+import org.objectweb.asm.util.Printer;
+import org.objectweb.asm.util.Textifier;
 import org.objectweb.asm.util.TraceClassVisitor;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 
 @IFMLLoadingPlugin.Name(value = "MyCoreMod") // The readable mod name
-@IFMLLoadingPlugin.MCVersion(value = "1.8")
-// The MC version it is designed for (Remember? Upwards/Downwards compatibility lost!)
-@IFMLLoadingPlugin.TransformerExclusions(value = "net.timeless.asm")
-// Your whole core mod package - Whatever you don't want the transformers to run over to prevent circularity Exceptions
-@IFMLLoadingPlugin.SortingIndex(value = Integer.MAX_VALUE)
-// How early your core mod is called - Use > 1000 to work with srg names
+@IFMLLoadingPlugin.MCVersion(value = "1.8") // The MC version it is designed for (Remember? Upwards/Downwards compatibility lost!)
+@IFMLLoadingPlugin.TransformerExclusions(value = "net.timeless.asm") // Your whole core mod package - Whatever you don't want the transformers to run over to prevent circularity Exceptions
+@IFMLLoadingPlugin.SortingIndex(value = Integer.MAX_VALUE) // How early your core mod is called - Use > 1000 to work with srg names
 public class ASMTransformer implements IClassTransformer
 {
 
@@ -26,14 +30,14 @@ public class ASMTransformer implements IClassTransformer
     public ASMTransformer()
     {
         folder = new File(".", "asm/debug");
-        if (!folder.exists())
+        if(!folder.exists())
             folder.mkdirs();
         filesToDebug = Lists.newArrayList();
         String options = System.getProperty("unilibAsmDebug");
-        if (options != null)
+        if(options != null)
         {
             String[] classes = options.split(";");
-            for (String clazz : classes)
+            for(String clazz : classes)
             {
                 filesToDebug.add(clazz.replace(".", "/"));
             }
@@ -46,11 +50,15 @@ public class ASMTransformer implements IClassTransformer
         // Unilib does not operate on the bytecode but only outputs the bytecode as text and as a class file inside the
         // gameFolder/debug/ folder
         String name = className.replace(".", "/");
-        if (name.equals("net/minecraft/item/ItemStack"))
+        if(name.equals("net/minecraft/item/ItemStack"))
         {
+            ClassReader reader = new ClassReader(bytes);
+            ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+            reader.accept(new UniItemStackTransformer(Opcodes.ASM5, writer), ClassReader.EXPAND_FRAMES);
+            bytes = writer.toByteArray();
             // TODO: Modify ItemStack::getMetadata()
         }
-        if (filesToDebug.contains(name))
+        if(filesToDebug.contains(name))
         {
             try
             {
@@ -74,10 +82,66 @@ public class ASMTransformer implements IClassTransformer
 
     private File createFile(File folder, String className, String extension) throws IOException
     {
-        File result = new File(folder, className.replace(".", "/") + "." + extension);
-        if (!result.getParentFile().exists())
+        File result = new File(folder,className.replace(".", "/")+"."+extension);
+        if(!result.getParentFile().exists())
             result.getParentFile().mkdirs();
         result.createNewFile();
         return result;
+    }
+
+    private class UniItemStackTransformer extends ClassVisitor
+    {
+        public UniItemStackTransformer(int api, ClassWriter writer)
+        {
+            super(api, writer);
+        }
+
+        @Override
+        public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions)
+        {
+            MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
+            if(name.equals("getItem"))
+            {
+                return new MetadataGetterTransformer(api, mv);
+            }
+            return mv;
+        }
+    }
+
+    private class MetadataGetterTransformer extends MethodVisitor implements Opcodes
+    {
+        private final Label endLabel;
+
+        public MetadataGetterTransformer(int api, MethodVisitor mv)
+        {
+            super(api, mv);
+            endLabel = new Label();
+        }
+
+        @Override
+        public void visitLabel(Label label)
+        {
+            super.visitLabel(label);
+        }
+
+        @Override
+        public void visitInsn(int opcode)
+        {
+            if(opcode == ARETURN)
+            {
+                super.visitInsn(DUP);
+                super.visitJumpInsn(IFNULL, endLabel);
+                super.visitInsn(ARETURN);
+                super.visitLabel(endLabel);
+                super.visitInsn(POP);
+                // Loads Items.stick
+                super.visitFieldInsn(GETSTATIC, "net/minecraft/init/Items", "stick", "Lnet/minecraft/item/Item;");
+                super.visitInsn(ARETURN);
+            }
+            else
+            {
+                super.visitInsn(opcode);
+            }
+        }
     }
 }
