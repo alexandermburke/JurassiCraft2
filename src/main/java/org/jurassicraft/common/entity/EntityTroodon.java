1b6b25e0aca9afb26f87bf59752caeb6b8aa802c
gegy1000 <gegy1000@gmail.com>
2015-10-24 20:50:48 +0200

    Oops

--- src/main/java/org/jurassicraft/common/entity/EntityTroodon.java
+++ src/main/java/org/jurassicraft/common/entity/EntityTroodon.java
@@ -1,11 +1,12 @@
 package org.jurassicraft.common.entity;
 
 import net.minecraft.world.World;
+import org.jurassicraft.common.entity.base.EntityDinosaurAggressive;
 import org.jurassicraft.common.entity.base.EntityDinosaurDefensiveHerbivore;
 
 import java.lang.Override;
 
-public class EntityTroodon extends EntityDinosaurDefensiveHerbivore
+public class EntityTroodon extends EntityDinosaurAggressive
 {
     public EntityTroodon(World world)
     {
