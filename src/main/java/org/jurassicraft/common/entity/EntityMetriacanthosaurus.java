c0f9031ead6385322153d15e3ce9cb90c507a24c
JTGhawk137 <jtghawk137@gmail.com>
2015-10-24 14:19:50 -0500

    attack targets

--- src/main/java/org/jurassicraft/common/entity/EntityMetriacanthosaurus.java
+++ src/main/java/org/jurassicraft/common/entity/EntityMetriacanthosaurus.java
@@ -1,16 +1,32 @@
 package org.jurassicraft.common.entity;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import org.jurassicraft.common.entity.base.EntityDinosaurAggressive;
 import org.jurassicraft.common.entity.base.EntityDinosaurDefensiveHerbivore;
 
 import java.lang.Override;
+import java.util.Random;
 
 public class EntityMetriacanthosaurus extends EntityDinosaurAggressive
 {
+
+    private static final Class[] targets = { EntityCompsognathus.class, EntityAnkylosaurus.class, EntityPlayer.class, EntityDilophosaurus.class, EntityDimorphodon.class, EntityDodo.class, EntityLeaellynasaura.class, EntityLudodactylus.class, EntityHypsilophodon.class, EntityGallimimus.class, EntitySegisaurus.class, EntityProtoceratops.class, EntityParasaurolophus.class, EntityOthnielia.class, EntityMicroceratus.class, EntityTriceratops.class, EntityStegosaurus.class };
+    private static final Class[] deftargets = { EntityPlayer.class, EntityTyrannosaurus.class, EntityGiganotosaurus.class, EntitySpinosaurus.class };
+
     public EntityMetriacanthosaurus(World world)
     {
         super(world);
+
+        for (int i = 0; i < targets.length; i++)
+        {
+            this.addAIForAttackTargets(targets[i], new Random().nextInt(3) + 1);
+        }
+
+        for (int j = 0; j < deftargets.length; j++)
+        {
+            this.defendFromAttacker(deftargets[j], new Random().nextInt(3) + 1);
+        }
     }
 
     @Override
--- src/main/java/org/jurassicraft/common/entity/EntityTroodon.java
+++ src/main/java/org/jurassicraft/common/entity/EntityTroodon.java
@@ -1,15 +1,33 @@
 package org.jurassicraft.common.entity;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import org.jurassicraft.common.entity.base.EntityDinosaurDefensiveHerbivore;
 
 import java.lang.Override;
+import java.util.Random;
 
 public class EntityTroodon extends EntityDinosaurDefensiveHerbivore
 {
+
+    private static final Class[] targets = { EntityCompsognathus.class, EntityPlayer.class, EntityDilophosaurus.class, EntityDimorphodon.class, EntityDodo.class, EntityLeaellynasaura.class, EntityHypsilophodon.class, EntitySegisaurus.class, EntityProtoceratops.class, EntityOthnielia.class, EntityMicroceratus.class };
+    private static final Class[] deftargets = { EntityPlayer.class, EntityTyrannosaurus.class, EntityGiganotosaurus.class, EntitySpinosaurus.class };
+
     public EntityTroodon(World world)
     {
         super(world);
+
+        this.addAIForAttackTargets(EntityPlayer.class, 1);
+
+        for (int i = 0; i < targets.length; i++)
+        {
+            this.addAIForAttackTargets(targets[i], new Random().nextInt(3) + 1);
+        }
+
+        for (int j = 0; j < deftargets.length; j++)
+        {
+            this.defendFromAttacker(deftargets[j], new Random().nextInt(3) + 1);
+        }
     }
 
     @Override
