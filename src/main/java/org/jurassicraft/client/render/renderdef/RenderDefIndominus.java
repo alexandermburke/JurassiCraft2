e05d7f7b965b7069f87a373533fdee981da61244
gegy1000 <gegy1000@gmail.com>
2016-01-05 10:43:52 +0200

    Fix Indominus rendering incorrectly

--- src/main/java/org/jurassicraft/client/render/renderdef/RenderDefIndominus.java
+++ src/main/java/org/jurassicraft/client/render/renderdef/RenderDefIndominus.java
@@ -1,8 +1,10 @@
 package org.jurassicraft.client.render.renderdef;
 
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import org.jurassicraft.client.model.animation.AnimationIndominus;
 import org.jurassicraft.client.render.entity.RenderIndominus;
+import org.jurassicraft.common.entity.base.EntityDinosaur;
 import org.jurassicraft.common.entity.base.JCEntityRegistry;
 
 public class RenderDefIndominus extends RenderDinosaurDefinition
@@ -12,7 +14,8 @@ public class RenderDefIndominus extends RenderDinosaurDefinition
         super(JCEntityRegistry.indominus, new AnimationIndominus(), adultScaleAdjustment, babyScaleAdjustment, parShadowSize, 0.0F, 0.0F, 0.0F);
     }
 
-    public RenderLiving getRenderer()
+    @Override
+    public Render<? super EntityDinosaur> createRenderFor()
     {
         return new RenderIndominus(this);
     }
