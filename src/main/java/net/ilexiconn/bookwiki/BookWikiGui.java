39bdb6681dca555ea21a2c13cedaa6c192de9b28
iLexiconn <ilexiconn@gmail.com>
2016-01-07 22:28:10 +0100

    BookWiki changes * Tweaked texture

--- src/main/java/net/ilexiconn/bookwiki/BookWikiGui.java
+++ src/main/java/net/ilexiconn/bookwiki/BookWikiGui.java
@@ -3,6 +3,7 @@ package net.ilexiconn.bookwiki;
 import com.google.common.collect.Lists;
 import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.item.ItemStack;
@@ -11,6 +12,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.opengl.GL11;
 
 import java.io.IOException;
 import java.util.List;
@@ -27,7 +29,6 @@ public class BookWikiGui extends GuiScreen {
 
     public Pattern recipePattern = Pattern.compile("<r:[A-Za-z0-9]+>");
     public Pattern colorPattern = Pattern.compile("<c:[A-Za-z]+>");
-    public String[] rainbowChars = new String[]{"4", "6", "e", "a", "9", "5"};
 
     public BookWikiGui(BookWiki bookWiki) {
         this.bookWiki = bookWiki;
@@ -51,24 +52,29 @@ public class BookWikiGui extends GuiScreen {
             int y = height / 2 - 180 / 2 - 24 + 7;
             mc.getTextureManager().bindTexture(BookWikiGui.TEXTURE);
             GlStateManager.disableLighting();
+            ItemStack stack = category.getIcon();
+            RenderHelper.enableGUIStandardItemLighting();
+            mc.getRenderItem().zLevel = 100.0F;
             if (currentCategory.equals(category.getID())) {
-                drawModalRectWithCustomSizedTexture(x, y, 346, 77, 27, 24, 512.0F, 512.0F);
+                drawModalRectWithCustomSizedTexture(x - 3, y, 374, 77, 27, 24, 512.0F, 512.0F);
+                startGlScissor(x + 3, y + 9, 16, 12);
+                mc.getRenderItem().renderItemAndEffectIntoGUI(stack, x + 3, y + 5);
+                mc.getRenderItem().renderItemOverlayIntoGUI(mc.fontRendererObj, stack, x + 3, y + 5, null);
+                endGlScissor();
             } else if (mouseX >= x && mouseY >= y && mouseX < x + 27 && mouseY < y + 24) {
                 drawModalRectWithCustomSizedTexture(x, y, 319, 77, 27, 24, 512.0F, 512.0F);
+                mc.getRenderItem().renderItemAndEffectIntoGUI(stack, x + 5, y + 3);
+                mc.getRenderItem().renderItemOverlayIntoGUI(mc.fontRendererObj, stack, x + 5, y + 3, null);
             } else {
                 drawModalRectWithCustomSizedTexture(x, y, 292, 77, 27, 24, 512.0F, 512.0F);
+                RenderHelper.enableGUIStandardItemLighting();
+                mc.getRenderItem().renderItemAndEffectIntoGUI(stack, x + 5, y + 5);
+                mc.getRenderItem().renderItemOverlayIntoGUI(mc.fontRendererObj, stack, x + 5, y + 5, null);
             }
+            mc.getRenderItem().zLevel = 0.0F;
+            RenderHelper.disableStandardItemLighting();
             if (mouseX >= x && mouseY >= y && mouseX < x + 27 && mouseY < y + 24) {
                 hover = category.getName();
-            }
-            ItemStack stack = category.getIcon();
-            if (stack != null) {
-                RenderHelper.enableGUIStandardItemLighting();
-                mc.getRenderItem().zLevel = 100.0F;
-                mc.getRenderItem().renderItemAndEffectIntoGUI(stack, x + 5, y + 5 + (hover != null && hover.equals(category.getName()) && !currentCategory.equals(category.getID()) ? -2 : 0));
-                mc.getRenderItem().renderItemOverlayIntoGUI(mc.fontRendererObj, stack, x + 5, y + 5 + (hover != null && hover.equals(category.getName()) && !currentCategory.equals(category.getID()) ? -2 : 0), null);
-                mc.getRenderItem().zLevel = 0.0F;
-                RenderHelper.disableStandardItemLighting();
             }
         }
         List<String> lines = Lists.newArrayList(fontRendererObj.listFormattedStringToWidth(getContent(bookWiki.getCategoryByID(currentCategory).getDefaultPage()), 116));
@@ -83,6 +89,7 @@ public class BookWikiGui extends GuiScreen {
                     BookWikiContainer.Recipe recipe = bookWiki.getRecipeByID(id);
                     recipeRenderer.render(recipe, width / 2 - 292 / 2 + 16 + 16, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * (i + 1), mouseX, mouseY);
                 }
+                GlStateManager.disableLighting();
                 fontRendererObj.drawString(line, width / 2 - 292 / 2 + 16, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * i, 0x000);
             } else {
                 if (matcher.find()) {
@@ -92,6 +99,7 @@ public class BookWikiGui extends GuiScreen {
                     BookWikiContainer.Recipe recipe = bookWiki.getRecipeByID(id);
                     recipeRenderer.render(recipe, width / 2 - 292 / 2 + 16 + 16 + 140, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * (i - 16), mouseX, mouseY);
                 }
+                GlStateManager.disableLighting();
                 fontRendererObj.drawString(line, width / 2 - 292 / 2 + 16 + 140, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * (i - 17), 0x000);
             }
         }
@@ -115,7 +123,6 @@ public class BookWikiGui extends GuiScreen {
             if (formatting != null) {
                 result = result.replace(group, formatting + "");
             }
-
         }
         return result;
     }
@@ -136,5 +143,17 @@ public class BookWikiGui extends GuiScreen {
                 }
             }
         }
+    }
+
+    public void startGlScissor(int x, int y, int width, int height) {
+        ScaledResolution scaledResolution = new ScaledResolution(mc);
+        double scaleW = (double) mc.displayWidth / scaledResolution.getScaledWidth_double();
+        double scaleH = (double) mc.displayHeight / scaledResolution.getScaledHeight_double();
+        GL11.glEnable(GL11.GL_SCISSOR_TEST);
+        GL11.glScissor((int) Math.floor((double) x * scaleW), (int) Math.floor((double) mc.displayHeight - ((double) (y + height) * scaleH)), (int) Math.floor((double) (x + width) * scaleW) - (int) Math.floor((double) x * scaleW), (int) Math.floor((double) mc.displayHeight - ((double) y * scaleH)) - (int) Math.floor((double) mc.displayHeight - ((double) (y + height) * scaleH))); //starts from lower left corner (minecraft starts from upper left)
+    }
+
+    public static void endGlScissor() {
+        GL11.glDisable(GL11.GL_SCISSOR_TEST);
     }
 }
