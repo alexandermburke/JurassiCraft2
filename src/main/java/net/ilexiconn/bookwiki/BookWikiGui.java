8f1f7bcc35ee80d825e4fa628d1600efba0bc023
iLexiconn <ilexiconn@gmail.com>
2016-01-07 21:26:23 +0100

    BookWiki changes * Added support for color codes * Centered recipe grid * Tweaked texture * Fixed recipe grid line offset * Fixed server crash

--- src/main/java/net/ilexiconn/bookwiki/BookWikiGui.java
+++ src/main/java/net/ilexiconn/bookwiki/BookWikiGui.java
@@ -1,10 +1,12 @@
 package net.ilexiconn.bookwiki;
 
+import com.google.common.collect.Lists;
 import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.fml.relauncher.Side;
@@ -24,6 +26,8 @@ public class BookWikiGui extends GuiScreen {
     private RecipeRenderer recipeRenderer;
 
     public Pattern recipePattern = Pattern.compile("<r:[A-Za-z0-9]+>");
+    public Pattern colorPattern = Pattern.compile("<c:[A-Za-z]+>");
+    public String[] rainbowChars = new String[]{"4", "6", "e", "a", "9", "5"};
 
     public BookWikiGui(BookWiki bookWiki) {
         this.bookWiki = bookWiki;
@@ -67,7 +71,7 @@ public class BookWikiGui extends GuiScreen {
                 RenderHelper.disableStandardItemLighting();
             }
         }
-        List<String> lines = fontRendererObj.listFormattedStringToWidth(bookWiki.getCategoryByID(currentCategory).getDefaultPage().getContent(), 116);
+        List<String> lines = Lists.newArrayList(fontRendererObj.listFormattedStringToWidth(getContent(bookWiki.getCategoryByID(currentCategory).getDefaultPage()), 116));
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
             Matcher matcher = recipePattern.matcher(line);
@@ -77,18 +81,43 @@ public class BookWikiGui extends GuiScreen {
                     line = line.replace(group, "");
                     String id = group.substring(3, group.length() - 1);
                     BookWikiContainer.Recipe recipe = bookWiki.getRecipeByID(id);
-                    fontRendererObj.drawString(line, width / 2 - 292 / 2 + 16, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * i, 0x000);
-                    recipeRenderer.render(recipe, width / 2 - 292 / 2 + 16, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * (i + 1), mouseX, mouseY);
-                    continue;
+                    recipeRenderer.render(recipe, width / 2 - 292 / 2 + 16 + 16, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * (i + 1), mouseX, mouseY);
                 }
                 fontRendererObj.drawString(line, width / 2 - 292 / 2 + 16, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * i, 0x000);
             } else {
+                if (matcher.find()) {
+                    String group = matcher.group();
+                    line = line.replace(group, "");
+                    String id = group.substring(3, group.length() - 1);
+                    BookWikiContainer.Recipe recipe = bookWiki.getRecipeByID(id);
+                    recipeRenderer.render(recipe, width / 2 - 292 / 2 + 16 + 16 + 140, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * (i - 16), mouseX, mouseY);
+                }
                 fontRendererObj.drawString(line, width / 2 - 292 / 2 + 16 + 140, height / 2 - 180 / 2 + 14 + fontRendererObj.FONT_HEIGHT * (i - 17), 0x000);
             }
         }
         if (hover != null) {
             drawCreativeTabHoveringText(StatCollector.translateToLocal(hover), mouseX, mouseY);
         }
+    }
+
+    public String getContent(BookWikiContainer.Page page) {
+        String result = page.getContent();
+        Matcher matcher = recipePattern.matcher(result);
+        while (matcher.find()) {
+            String group = matcher.group();
+            result = result.replace(group, group + "\n\n\n\n\n\n\n");
+        }
+        matcher = colorPattern.matcher(result);
+        while (matcher.find()) {
+            String group = matcher.group();
+            String id = group.substring(3, group.length() - 1).toUpperCase();
+            EnumChatFormatting formatting = EnumChatFormatting.getValueByName(id);
+            if (formatting != null) {
+                result = result.replace(group, formatting + "");
+            }
+
+        }
+        return result;
     }
 
     @Override
@@ -103,7 +132,7 @@ public class BookWikiGui extends GuiScreen {
                 int y = height / 2 - 180 / 2 - 24 + 7;
                 if (mouseX >= x && mouseY >= y && mouseX < x + 27 && mouseY < y + 24) {
                     currentCategory = category.getID();
-                    mc.getSoundHandler().playSound(PositionedSoundRecord.create(new ResourceLocation("jurassicraft:page-flip"), 1.0F));
+                    mc.getSoundHandler().playSound(PositionedSoundRecord.create(new ResourceLocation(bookWiki.getMod().modid() + ":page-flip"), 1.0F));
                 }
             }
         }
